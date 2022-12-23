045625530d2bc2ce9ea8d551681a393aa9f450ef
wilu23 <wilek23@gmail.com>
2022-12-23 10:05:17 +0100

    Update ar.ps1
    1

--- ar.ps1
+++ ar.ps1
@@ -1,8 +1,11 @@
-# Ustawienie nazwy programu, który chcemy zainstalować.
-$programName = "D:\Instrukcje\BAT\AcroRdrDC1900820071_pl_PL.exe"
+# Pobierz najnowszą wersję programu Adobe Reader w języku polskim
+$url = "https://get.adobe.com/pl/reader/direct/readerdc.exe"
+$output = "C:\Temp\readerdc.exe"
 
-# Uruchomienie polecenia Start-Process z opcją -Force, aby zainstalować program.
-Install-Package -Name $programName -Force
+Invoke-WebRequest -Uri $url -OutFile $output
 
-# Potwierdzenie zakończenia instalacji programu.
-Write-Host "Zainstalowano program $programName."
+# Uruchom instalator programu Adobe Reader
+Start-Process -FilePath $output -ArgumentList "/sAll /rs /msi EULA_ACCEPT=YES"
+
+# Usuń plik instalatora po zakończeniu instalacji
+Remove-Item -Path $output
